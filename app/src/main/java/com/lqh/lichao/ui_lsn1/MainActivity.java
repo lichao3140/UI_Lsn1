41133e5de0165f83e09e27add8e3af146a2304b8
lichao3140 <396229938@qq.com>
2017-10-05 21:30:13 +0800

    高级UI瀑布流布局

---
+++ app/src/main/java/com/lqh/lichao/ui_lsn1/MainActivity.java
@@ -0,0 +1,16 @@
+package com.lqh.lichao.ui_lsn1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * 瀑布流布局
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
